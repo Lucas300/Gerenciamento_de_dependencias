bf259128c4ce941ce172bc2b7574da1f02d56af2
Lucas Daniel <ludaniel.sd@gmail.com>
2024-07-26 23:57:21 -0300

    Adicionando o codigo mockito comentado

--- quick-start-maven/src/test/java/one/digitalinnovation/mockito/CadastrarPessoaTeste.java
+++ quick-start-maven/src/test/java/one/digitalinnovation/mockito/CadastrarPessoaTeste.java
@@ -1,5 +1,28 @@
 package one.digitalinnovation.mockito;
 
-public class CadastrarPessoaTeste {
+import java.time.LocalDate;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+
+// ExtendWith(MokitoExtension.class)
+public class CadastrarPessoaTeste {
+/*
+	@Mock
+	private ApiDosCorreios apiDosCorreios;
+	
+	@InjectMocks
+	private CadastrarPessoa cadastraPessoa;
+	
+	@Test
+	void validarDadosCadastro() {
+		DadosLocalizado dadoslocalizacao = new DadosLocalizacao("mg","Itaqua","rua lago das flores","vila do dinossauros",242);
+		Mockito.when(apiDosCorreios.buscarDadosComBaseNoCep("798789")).thenReturn(dadoslocalizacao);
+		Pessoa willyan  cadastraPessoa.cadastrarPessoa("willyan", "798789", LocalDate.now(), 0);
+	}
+
+*/
 }
